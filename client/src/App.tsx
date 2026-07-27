ae01fe93ebb6e862a034299185c44d21c4d2c948
Vanessa Victorino <vanessaf.victorino@yahoo.com>
2026-07-27 10:49:20 -0500

    feat: initialize React TypeScript client

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1>Tomorrow Is Calling</h1>
+      <p>Temporal communications system offline.</p>
+    </main>
+  );
+}
+
+export default App;
